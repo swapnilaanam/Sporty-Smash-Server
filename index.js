54b62d82667430bbdd40943407b67416e469d0c9
swapnilaanam <swapnilaanam211@gmail.com>
2023-06-10 14:27:52 +0600

    added a post api at /users for creating new users

--- index.js
+++ index.js
@@ -25,6 +25,24 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const userCollection = client.db("sportyDB").collection("users");
+
+        // users related apis
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+
+            const existingUser = await userCollection.findOne(query);
+
+            if (existingUser) {
+                return res.send({ message: 'User Already Exists...' })
+            }
+
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
